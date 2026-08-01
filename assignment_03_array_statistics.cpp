ad5ea91e6c904ec0370139f60a8ab1b4bc922015
nicho2005 <nicholasamoah800@gmail.com>
2026-08-01 21:52:14 +0000

    complete array statistics

--- assignment_03_array_statistics.cpp
+++ assignment_03_array_statistics.cpp
@@ -42,3 +42,82 @@
 #include <iostream>
 using namespace std;
 
+
+int findSum(int arr[], int size)
+{
+    int sum = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+double findAverage(int arr[], int size)
+{
+    int sum = findSum(arr, size);
+    return (double)sum / size;
+}
+
+int findMaximum(int arr[], int size)
+{
+    int max = arr[0];
+
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+
+int findMinimum(int arr[], int size)
+{
+    int min = arr[0];
+
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+
+    return min;
+}
+
+int main()
+{
+    int n;
+
+    cout << "How many numbers? ";
+    cin >> n;
+
+    if (n <= 0)
+    {
+        cout << "Error: Number of values must be greater than 0." << endl;
+        return 0;
+    }
+
+    int numbers[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter number " << i + 1 << ": ";
+        cin >> numbers[i];
+    }
+
+    cout << "\nResults:" << endl;
+    cout << "Sum: " << findSum(numbers, n) << endl;
+    cout << "Average: " << findAverage(numbers, n) << endl;
+    cout << "Maximum: " << findMaximum(numbers, n) << endl;
+    cout << "Minimum: " << findMinimum(numbers, n) << endl;
+
+    return 0;
+}
