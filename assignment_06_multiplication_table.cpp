4afb7861a75c087100588e872d4a3cce31b87bc7
nicho2005 <nicholasamoah800@gmail.com>
2026-08-01 22:36:07 +0000

    complete multiplication table

--- assignment_06_multiplication_table.cpp
+++ assignment_06_multiplication_table.cpp
@@ -57,3 +57,62 @@
 #include <iostream>
 using namespace std;
 
+
+
+
+void printTable(int number)
+{
+    cout << "\nMultiplication Table for " << number << ":\n";
+
+    for (int i = 1; i <= 12; i++)
+    {
+        cout << number << " x " << i << " = " << number * i << endl;
+    }
+}
+
+
+void printTables(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        printTable(i);
+
+        if (i < n)
+        {
+            cout << "---------------------------" << endl;
+        }
+    }
+}
+
+int main()
+{
+    int number, n;
+
+    // Part A
+    cout << "PART A - Single Multiplication Table" << endl;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number <= 0)
+    {
+        cout << "Error: Please enter a positive integer." << endl;
+        return 0;
+    }
+
+    printTable(number);
+
+    // Part B
+    cout << "\nPART B - Multiplication Tables from 1 to N" << endl;
+    cout << "Enter a number N: ";
+    cin >> n;
+
+    if (n <= 0)
+    {
+        cout << "Error: Please enter a positive integer." << endl;
+        return 0;
+    }
+
+    printTables(n);
+
+    return 0;
+}
